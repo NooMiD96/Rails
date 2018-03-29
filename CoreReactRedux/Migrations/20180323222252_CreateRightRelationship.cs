7319e2b7b21eb104464ebe28d92f5a7e1759b083
Дмитрий <koshak-9696@inbox.ru>
2018-03-30 00:35:29 +0300

    Add new rail and rename old one

---
+++ CoreReactRedux/Migrations/20180323222252_CreateRightRelationship.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreReactRedux.Migrations
+{
+    public partial class CreateRightRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChildId",
+                table: "DataClassParentTable");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChildId",
+                table: "DataClassParentTable",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
