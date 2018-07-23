96351a367f00797d0e96e22b4019e0ea732d5e99
Dmitriy <dkoshkin@smartru.com>
2018-07-23 17:10:19 +0300

    Add url change handler

--- CoreReactReduxTypeScript/ClientApp/src/Components/NavMenu.tsx
+++ CoreReactReduxTypeScript/ClientApp/src/Components/NavMenu.tsx
@@ -1,33 +1,61 @@
 import * as React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Menu from "@core/Menu";
 
-export class NavMenu extends React.Component<{}, {}> {
+interface IState {
+    selectedKeys: string[];
+}
+
+export class NavMenu extends React.Component<{}, IState> {
+    state = {
+        selectedKeys: ["1"],
+    };
+
     componentDidMount() {
-        //
+        const urls = document.location.pathname.split("/").filter(Boolean);
+        if (urls.length) {
+            let urlKey = "1";
+            switch (urls[0].toLowerCase()) {
+                case "fetcher":
+                    urlKey = "2";
+                    break;
+                case "counter":
+                    urlKey = "3";
+                    break;
+                default:
+                    break;
+            }
+            this.setState({
+                selectedKeys: [urlKey],
+            });
+        }
     }
+
+    onSelectItemHandler = (event: { item: {}, key: string, selectedKeys: string[] }) => {
+        if (!this.state.selectedKeys.includes(event.key)) {
+            this.setState({
+                selectedKeys: [event.key],
+            });
+        }
+    }
+
     public render() {
         return (
             <div>
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={["1"]}
+                    selectedKeys={this.state.selectedKeys}
+                    onSelect={this.onSelectItemHandler}
                 >
                     <Menu.Item key="1">
-                        <NavLink exact to={"/"}>
-                            <span className="glyphicon glyphicon-home"></span> Home
-                        </NavLink>
+                        <Link to={"/"}>Home</Link>
                     </Menu.Item>
                     <Menu.Item key="2">
-                        <NavLink to={"/Fetcher"}>
-                            <span className="glyphicon glyphicon-home"></span> Fetcher
-                        </NavLink>
+                        <Link to={"/Fetcher"}>Fetcher</Link>
                     </Menu.Item>
                     <Menu.Item key="3">
-                        <NavLink to={"/Counter"}>
-                            <span className="glyphicon glyphicon-home"></span> Counter
-                        </NavLink>
+                        <Link to={"/Counter"}>Counter</Link>
                     </Menu.Item>
                 </Menu>
             </div>
