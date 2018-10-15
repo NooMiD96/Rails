6b83960925390c85aec9587f902eb4a9f8d14724
Dmitriy Koshkin <dkoshkin@smartru.com>
2018-10-15 12:11:51 +0300

    Splite the antd's icons

--- CoreReactReduxTypeScript/ClientApp/src/core/antd/Icon.tsx
+++ CoreReactReduxTypeScript/ClientApp/src/core/antd/Icon.tsx
@@ -1,155 +1,45 @@
-// import * as React from "react";
-// import Svg, { IconDefinition } from "./Svg";
-// import { mainIcons, icons, TIcons, TMainIcons } from "../constants";
-
-// export interface CustomIconProps {
-//   type: TIcons | TMainIcons;
-//   className?: string;
-// }
-
-// export interface CustomIconState {
-//   svgProps: IconDefinition | null;
-// }
-
-// class CustomIcon extends React.PureComponent<CustomIconProps, CustomIconState> {
-//   state: CustomIconState = {
-//     svgProps: null,
-//   };
-
-//   importSvg = async (path: string) => {
-//     const svg = await import(/* webpackChunkName: "icons" */ path);
-//     return svg.default as IconDefinition;
-//   }
-
-//   importMainSvg = async (path: string) => {
-//     const svg = await import(
-//       /* webpackChunkName: "icons" */
-//       path
-//     );
-//     return svg.default as IconDefinition;
-//   }
-
-//   loadIcon = async (type: TIcons | TMainIcons) => {
-//     if (icons[type as TIcons]) {
-//       const svgProps = await this.importSvg(icons[type as TIcons]);
-//       this.setState({svgProps});
-//     } else if (mainIcons[type as TMainIcons]) {
-//       const svgProps = await this.importMainSvg(mainIcons[type as TMainIcons]);
-//       this.setState({svgProps});
-//     } else {
-//       throw new Error(`SVG with "${type}" type not found!`);
-//     }
-//   }
-//   async componentDidMount() {
-//     const { type } = this.props;
-//     try {
-//       await this.loadIcon(type);
-//     } catch (err) {
-//       if (process.env.NODE_ENV === "development") {
-//         throw new Error(err.message);
-//       }
-//     }
-//   }
-
-//   render() {
-//     const { svgProps } = this.state;
-//     return (
-//       svgProps
-//         ? <Svg
-//           className={this.props.className}
-//           svgProps={svgProps}
-//         />
-//         : <span />
-//     );
-//   }
-// }
-
-// export default CustomIcon;
-
 import * as React from "react";
 import Svg, { IconDefinition } from "./Svg";
-
-import LoginSvgProps from "@antdSvgs/LoginOutline";
-import LogoutSvgProps from "@antdSvgs/LogoutOutline";
-import IdcardSvgProps from "@antdSvgs/IdcardOutline";
-import UserSvg from "@antdSvgs/UserOutline";
-import LockSvg from "@antdSvgs/LockOutline";
-import MailSvg from "@antdSvgs/MailOutline";
-import DownCircleSvg from "@antdSvgs/DownCircleOutline";
-import UpCircleSvg from "@antdSvgs/UpCircleOutline";
-import LeftSvg from "@antdSvgs/LeftOutline";
-import RightSvg from "@antdSvgs/RightOutline";
-import LoadingSvg from "@antdSvgs/LoadingOutline";
-import CloseCircleSvg from "@antdSvgs/CloseCircleOutline";
-import CloseSvg from "@antdSvgs/CloseOutline";
+import { getIconAsync, TIcons } from "../constants";
 
 export interface CustomIconProps {
-  type: string;
+  type: TIcons;
   className?: string;
 }
 
-const CustomIcon = (props: CustomIconProps): JSX.Element => {
-  const {
-    type = "",
-    className = "",
-  } = props;
+export interface CustomIconState {
+  svgProps: IconDefinition | null;
+}
 
-  let svgProps: IconDefinition;
-  switch (type) {
-    case "user":
-      svgProps = UserSvg;
-      break;
-    case "lock":
-      svgProps = LockSvg;
-      break;
-    case "mail":
-      svgProps = MailSvg;
-      break;
-    case "down-circle-o":
-      svgProps = DownCircleSvg;
-      break;
-    case "up-circle-o":
-      svgProps = UpCircleSvg;
-      break;
-    case "login":
-      svgProps = LoginSvgProps;
-      break;
-    case "idcard":
-      svgProps = IdcardSvgProps;
-      break;
-    case "logout":
-      svgProps = LogoutSvgProps;
-      break;
-    case "left":
-      svgProps = LeftSvg;
-      break;
-    case "right":
-      svgProps = RightSvg;
-      break;
-    case "loading":
-      svgProps = LoadingSvg;
-      break;
-    case "close-circle":
-      svgProps = CloseCircleSvg;
-      break;
-    case "close":
-      svgProps = CloseSvg;
-      break;
+class CustomIcon extends React.PureComponent<CustomIconProps, CustomIconState> {
+  state: CustomIconState = {
+    svgProps: null,
+  };
 
-    default:
+  async componentDidMount() {
+    const { type } = this.props;
+    try {
+      const svgProps = await getIconAsync(type);
+
+      this.setState({svgProps: svgProps});
+    } catch (err) {
       if (process.env.NODE_ENV === "development") {
-        throw new Error(`SVG with "${type}" type not found!`);
-      } else {
-        return <span />;
+        throw new Error(err.message);
       }
+    }
   }
 
-  return (
-    <Svg
-      className={className}
-      svgProps={svgProps}
-    />
-  );
-};
+  render() {
+    const { svgProps } = this.state;
+    return (
+      svgProps
+        ? <Svg
+          className={this.props.className}
+          svgProps={svgProps}
+        />
+        : <span />
+    );
+  }
+}
 
 export default CustomIcon;
--- CoreReactReduxTypeScript/ClientApp/src/core/constants.ts
+++ CoreReactReduxTypeScript/ClientApp/src/core/constants.ts
@@ -20,38 +20,72 @@ export enum UserTypeEnums {
   Admin,
 }
 
-export const mainIcons = {
+export type TIcons =
+  "loading"
   /* Main: Button */
-  "login": "@antdSvgs/LoginOutline",
-  "idcard": "@antdSvgs/IdcardOutline",
-  "logout": "@antdSvgs/LogoutOutline",
+  | "login"
+  | "idcard"
+  | "logout"
   /* END Main: Button */
-};
-
-export const icons = {
   /* Main: Account: Input */
-  "user": "@antdSvgs/UserOutline",
-  "lock": "@antdSvgs/LockOutline",
-  "mail": "@antdSvgs/MailOutline",
+  | "user"
+  | "lock"
+  | "mail"
   /* END Main: Account: Input */
-
   /* Alert */
-  "close-circle": "@antdSvgs/CloseCircleOutline",
-  "close": "@antdSvgs/CloseOutline",
+  | "close-circle"
+  | "close"
   /* END Alert */
-
   /* Input */
-  "left": "@antdSvgs/LeftOutline",
-  "right": "@antdSvgs/RightOutline",
+  | "left"
+  | "right"
   /* END Input */
-
   /* Counter: Button */
-  "down-circle-o": "@antdSvgs/DownCircleOutline",
-  "up-circle-o": "@antdSvgs/UpCircleOutline",
+  | "down-circle-o"
+  | "up-circle-o"
   /* END Counter: Button */
+  ;
 
-  "loading": "@antdSvgs/LoadingOutline",
+export const getIconAsync = async (type: TIcons) => {
+  switch (type) {
+    case "loading":
+      return (await import(/* webpackChunkName: "main-client.global.icons" */ "@antdSvgs/LoadingOutline")).default;
+/* Main: Button */
+    case "login":
+      return (await import(/* webpackChunkName: "main-client.icons" */ "@antdSvgs/LoginOutline")).default;
+    case "idcard":
+      return (await import(/* webpackChunkName: "main-client.icons" */ "@antdSvgs/IdcardOutline")).default;
+    case "logout":
+      return (await import(/* webpackChunkName: "main-client.icons" */ "@antdSvgs/LogoutOutline")).default;
+/* END Main: Button */
+/* Main: Account: Input */
+    case "user":
+      return (await import(/* webpackChunkName: "account.modal.icons" */ "@antdSvgs/UserOutline")).default;
+    case "lock":
+      return (await import(/* webpackChunkName: "account.modal.icons" */ "@antdSvgs/LockOutline")).default;
+    case "mail":
+      return (await import(/* webpackChunkName: "account.modal.icons" */ "@antdSvgs/MailOutline")).default;
+/* END Main: Account: Input */
+/* Alert */
+    case "close-circle":
+      return (await import(/* webpackChunkName: "alert.icons" */ "@antdSvgs/CloseCircleOutline")).default;
+    case "close":
+      return (await import(/* webpackChunkName: "alert.icons" */ "@antdSvgs/CloseOutline")).default;
+/* END Alert */
+/* Input */
+    case "left":
+      return (await import(/* webpackChunkName: "input.icons" */ "@antdSvgs/LeftOutline")).default;
+    case "right":
+      return (await import(/* webpackChunkName: "input.icons" */ "@antdSvgs/RightOutline")).default;
+/* END Input */
+/* Counter: Button */
+    case "down-circle-o":
+      return (await import(/* webpackChunkName: "counter.icons" */ "@antdSvgs/DownCircleOutline")).default;
+    case "up-circle-o":
+      return (await import(/* webpackChunkName: "counter.icons" */ "@antdSvgs/UpCircleOutline")).default;
+/* END Counter: Button */
+    default:
+      const exhaustiveCheck: never = type;
+      throw new Error(`SVG with "${exhaustiveCheck}" type not found!`);
+  }
 };
-
-export type TIcons = keyof typeof icons;
-export type TMainIcons = keyof typeof mainIcons;
